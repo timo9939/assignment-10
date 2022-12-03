27b139cf9f44219c3b65b0c6a414f7e6c819f2a5
timo9939 <timothylau20199@gmail.com>
2022-12-03 14:29:53 +0000

    inquirer.prompt: command line inpute upload

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-const jest=requie('jest')
+const jest=require('jest')
 const fs=require('fs')
+const inquirer=require('inquirer')
+
+
+
+inquirer.prompt([
+    {
+        type:'input',
+        name:'manager-name',
+        message:"Input the manager's name"
+    },
+
+    {
+        type:'list',
+        name:'role-option',
+        message:'Choose the role of the team member',
+        choices:['Engineer','Intern']
+
+    },
+
+    {
+        type:'input',
+        name:'ID',
+        message:'Input your ID number'
+    },
+
+    {
+        type:'input',
+        name:'email',
+        message:'Input yor email address'
+    },
+
+    {
+        type:'office number',
+        name:'office-num',
+        message:'Input your office number'
+    },
+ 
+    {
+        type:'input',
+        name:'gitHub',
+        message:'Input your Github number'
+
+    }
+])
